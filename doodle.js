92f5f5cff5d69f65fe673f506aa0c6ca474c358f
ishdutt <trivediishdutt@gmail.com>
2018-08-24 21:36:12 +0530

    Fake google

---
+++ doodle.js
@@ -0,0 +1,129 @@
+var button=document.querySelector("button");
+button.addEventListener("click",function(){
+	var c=document.querySelector("input").value;
+	if(c==="Mitashi Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?q=Mitashi+Sales+and+Service+center+-+System+Designing+919898368188&oq=Mitashi+Sales+and+Service+center+-+System+Designing+919898368188&aqs=chrome..69i57.4156j0j9&sourceid=chrome&ie=UTF-8');
+	}
+	if(c==="Electrolux Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?safe=active&ei=C_1_W8TUKcPLrQHUk63ACw&q=2+Electrolux+Sales+and+Service+center+-+System+Designing+919898368188&oq=2+Electrolux+Sales+and+Service+center+-+System+Designing+919898368188&gs_l=psy-ab.3...6161203.6162185.0.6163552.2.2.0.0.0.0.110.110.0j1.2.0....0...1c.1.64.psy-ab..0.1.116.6..35i39k1.116.xEMsE1nBEa8');
+	}
+	if(c==="Ogeneral Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?safe=active&ei=gRiAW-G-LJWm9QOr6ZII&q=Ogeneral+Sales+and+Service+center+-+System+Designing+919898368188&oq=Ogeneral+Sales+and+Service+center+-+System+Designing+919898368188&gs_l=psy-ab.3...4929.5119.0.6194.2.2.0.0.0.0.128.243.0j2.2.0....0...1c.1.64.psy-ab..0.0.0....0.nG1IHeRxznQ');
+	}
+	if(c==="Mitsubishi Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?safe=active&ei=nPt_W9vlCYqoyAOp6JzYCg&q=3+Mitsubishi+Sales+and+Service+center+-+System+Designing+919898368188&oq=3+Mitsubishi+Sales+and+Service+center+-+System+Designing+919898368188&gs_l=psy-ab.3...3613.365698.0.366597.10.9.1.0.0.0.141.834.3j5.9.0....0...1c.1.64.psy-ab..0.8.732.0..0j35i39k1j0i131k1j0i67k1j0i20i263k1j0i10k1.84.NPbPDCj1CPo');
+	}
+	if(c==="Hitachi Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?safe=active&ei=iBiAW4LWNMPLrQHUk63ACw&q=5+Hitachi+Sales+and+Service+center+-+System+Designing+919898368188&oq=5+Hitachi+Sales+and+Service+center+-+System+Designing+919898368188&gs_l=psy-ab.3...59165.60032.0.60868.2.2.0.0.0.0.108.108.0j1.2.0....0...1c.1.64.psy-ab..0.1.114.6..35i39k1.114.ryBYx668exs');
+	}
+	if(c==="Voltas Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?q=6+Voltas+Sales+and+Service+center+-+System+Designing+919898368188&oq=6+Voltas+Sales+and+Service+center+-+System+Designing+919898368188&aqs=chrome..69i57.1353j0j9&sourceid=chrome&ie=UTF-8');
+	}
+	if(c==="Haier Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?q=7+Haier+Sales+and+Service+center+-+System+Designing+919898368188&oq=7+Haier+Sales+and+Service+center+-+System+Designing+919898368188&aqs=chrome..69i57.1516j0j9&sourceid=chrome&ie=UTF-8');
+	}
+	if(c==="Carrier Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?q=8+Carrier+Sales+and+Service+center+-+System+Designing+919898368188&oq=8+Carrier+Sales+and+Service+center+-+System+Designing+919898368188&aqs=chrome..69i57.1655j0j9&sourceid=chrome&ie=UTF-8');
+	}
+	if(c==="Panasonic Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?q=9+Panasonic+Sales+and+Service+center+-+System+Designing+919898368188&oq=9+Panasonic+Sales+and+Service+center+-+System+Designing+919898368188&aqs=chrome..69i57.1329j0j9&sourceid=chrome&ie=UTF-8');
+	}
+	if(c==="Godrej Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?q=10+Godrej+Sales+and+Service+center+-+System+Designing+919898368188&oq=10+Godrej+Sales+and+Service+center+-+System+Designing+919898368188&aqs=chrome..69i57.1804j0j9&sourceid=chrome&ie=UTF-8');
+	}
+	if(c==="Lloyd Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?safe=active&ei=aBqAW93nFI_5rQGPmqSoBg&q=+Lloyd+Sales+and+Service+center+-+System+Designing+919898368188&oq=+Lloyd+Sales+and+Service+center+-+System+Designing+919898368188&gs_l=psy-ab.3...7746.7932.0.8705.2.2.0.0.0.0.113.224.0j2.2.0....0...1c.1.64.psy-ab..0.0.0....0.basW4Btc_ec');
+	}
+	if(c==="IFB Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?q=12+IFB+Sales+and+Service+center+-+System+Designing+919898368188&oq=12+IFB+Sales+and+Service+center+-+System+Designing+919898368188&aqs=chrome..69i57.1349j0j9&sourceid=chrome&ie=UTF-8');
+	}
+	if(c==="Kenstar Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?q=13+Kenstar+Sales+and+Service+center+-+System+Designing+919898368188&oq=13+Kenstar+Sales+and+Service+center+-+System+Designing+919898368188&aqs=chrome..69i57.22600j0j9&sourceid=chrome&ie=UTF-8');
+	}
+		if(c==="Videocon Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?q=14+Videocon+Sales+and+Service+center+-+System+Designing+919898368188&oq=14+Videocon+Sales+and+Service+center+-+System+Designing+919898368188&aqs=chrome..69i57.1661j0j9&sourceid=chrome&ie=UTF-8');
+	}
+		if(c==="Samsung Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?q=15+Samsung+Sales+and+Service+center+-+System+Designing+919898368188&oq=15+Samsung+Sales+and+Service+center+-+System+Designing+919898368188&aqs=chrome..69i57.1516j0j9&sourceid=chrome&ie=UTF-8');
+	}
+		if(c==="Lg Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?q=16+Lg+Sales+and+Service+center+-+System+Designing+919898368188&oq=16+Lg+Sales+and+Service+center+-+System+Designing+919898368188&aqs=chrome..69i57.1223j0j9&sourceid=chrome&ie=UTF-8');
+	}
+		if(c===" Onida Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?q=17+Onida+Sales+and+Service+center+-+System+Designing+919898368188&oq=17+Onida+Sales+and+Service+center+-+System+Designing+919898368188&aqs=chrome..69i57.1505j0j9&sourceid=chrome&ie=UTF-8');
+	}
+		if(c==="Hyundai Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?q=18+Hyundai+Sales+and+Service+center+-+System+Designing+919898368188&oq=18+Hyundai+Sales+and+Service+center+-+System+Designing+919898368188&aqs=chrome..69i57.1931j0j9&sourceid=chrome&ie=UTF-8');
+	}
+		if(c==="Bluestar Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?q=19+Bluestar+Sales+and+Service+center+-+System+Designing+919898368188&oq=19+Bluestar+Sales+and+Service+center+-+System+Designing+919898368188&aqs=chrome..69i57.1169j0j9&sourceid=chrome&ie=UTF-8');
+	}
+		if(c==="Whirlpool Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?q=20+Whirlpool+Sales+and+Service+center+-+System+Designing+919898368188&oq=20+Whirlpool+Sales+and+Service+center+-+System+Designing+919898368188&aqs=chrome..69i57.1425j0j9&sourceid=chrome&ie=UTF-8');
+	}
+		if(c==="Kelvinator Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?q=21+Kelvinator+Sales+and+Service+center+-+System+Designing+919898368188&oq=21+Kelvinator+Sales+and+Service+center+-+System+Designing+919898368188&aqs=chrome..69i57.1079j0j9&sourceid=chrome&ie=UTF-8');
+	}
+	if(c==="Sansui Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?q=22+Sansui+Sales+and+Service+center+-+System+Designing+919898368188&oq=22+Sansui+Sales+and+Service+center+-+System+Designing+919898368188&aqs=chrome..69i57.1299j0j9&sourceid=chrome&ie=UTF-8');
+	}
+	if(c==="Micromax Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?q=23+Micromax+Sales+and+Service+center+-+System+Designing+919898368188&oq=23+Micromax+Sales+and+Service+center+-+System+Designing+919898368188&aqs=chrome..69i57.1156j0j9&sourceid=chrome&ie=UTF-8');
+	}
+	if(c==="TCL Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?q=24+TCL+Sales+and+Service+center+-+System+Designing+919898368188&oq=24+TCL+Sales+and+Service+center+-+System+Designing+919898368188&aqs=chrome..69i57.1633j0j9&sourceid=chrome&ie=UTF-8');
+	}
+		if(c==="Midea Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?q=25+Midea+Sales+and+Service+center+-+System+Designing+919898368188&oq=25+Midea+Sales+and+Service+center+-+System+Designing+919898368188&aqs=chrome..69i57.2297j0j9&sourceid=chrome&ie=UTF-8');
+	}
+		if(c==="Toshiba Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?q=26+Toshiba+Sales+and+Service+center+-+System+Designing+919898368188&oq=26+Toshiba+Sales+and+Service+center+-+System+Designing+919898368188&aqs=chrome..69i57.2381j0j9&sourceid=chrome&ie=UTF-8');
+	}
+	if(c==="Trane Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?q=27+Trane+Sales+and+Service+center+-+System+Designing+919898368188&oq=27+Trane+Sales+and+Service+center+-+System+Designing+919898368188&aqs=chrome..69i57.1334j0j9&sourceid=chrome&ie=UTF-8');
+	}
+			if(c==="Daikin Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?q=28+Daikin+Sales+and+Service+center+-+System+Designing+919898368188&oq=28+Daikin+Sales+and+Service+center+-+System+Designing+919898368188&aqs=chrome..69i57.1944j0j9&sourceid=chrome&ie=UTF-8');
+	}
+			if(c==="Mitsubishi Heavy Sales and Service center - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?q=29+Mitsubishi+Heavy+Sales+and+Service+center+-+System+Designing+919898368188&oq=29+Mitsubishi+Heavy+Sales+and+Service+center+-+System+Designing+919898368188&aqs=chrome..69i57.1395j0j9&sourceid=chrome&ie=UTF-8');
+	}
+			if(c==="VRF / VRV Air Conditioning System - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?q=30+VRF+%2F+VRV+Air+Conditioning+System+-+System+Designing+919898368188&oq=30+VRF+%2F+VRV+Air+Conditioning+System+-+System+Designing+919898368188&aqs=chrome..69i57.1345j0j9&sourceid=chrome&ie=UTF-8');
+	}
+	if(c==="Ductable Air Conditioning System - System Designing 919898368188")
+	{
+		window.location.replace('https://www.google.com/search?q=31+Ductable+Air+Conditioning+System+-+System+Designing+919898368188&oq=31+Ductable+Air+Conditioning+System+-+System+Designing+919898368188&aqs=chrome..69i57.1290j0j9&sourceid=chrome&ie=UTF-8');
+	}
+	
+})
